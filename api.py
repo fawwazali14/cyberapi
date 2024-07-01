b3a72d42803745a854dcb3c609a9759346d284fe
fawwazali14 <fawwazali10@gmail.com>
2024-07-01 10:08:47 -0500

    ..

--- api.py
+++ api.py
@@ -12,7 +12,7 @@ from googleapiclient.discovery import build
 import pickle
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "https://cyber-censor.vercel.app"}})
+CORS(app)
 
 @app.route('/ml', methods=['POST'])
 def ml():
